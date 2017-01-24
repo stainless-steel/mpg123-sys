8dafeef55de38d7fd777a5432a4341a1125c39f4
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-01-24 18:55:53 +0100

    Add a feature for linking statically

--- build.rs
+++ build.rs
@@ -19,14 +19,19 @@ fn main() {
     if pkg_config::find_library("mpg123").is_ok() {
         return;
     }
+    let dynamic = env::var("CARGO_FEATURE_STATIC").is_err();
     let source = PathBuf::from(&get!("CARGO_MANIFEST_DIR")).join("source");
     let output = PathBuf::from(&get!("OUT_DIR"));
     let build = output.join("build");
     ok!(fs::create_dir_all(&build));
     run!(cmd!(source.join("configure")).current_dir(&build)
+                                       .arg(&format!("--{}-shared",
+                                                     if dynamic { "enable" } else { "disable" }))
+                                       .arg(&format!("--{}-static",
+                                                     if dynamic { "disable" } else { "enable" }))
                                        .arg(&format!("--prefix={}", output.display())));
     run!(cmd!("make").current_dir(&build).arg("install"));
     println!("cargo:root={}", output.display());
-    println!("cargo:rustc-link-lib=dylib=mpg123");
+    println!("cargo:rustc-link-lib={}=mpg123", if dynamic { "dylib" } else { "static" });
     println!("cargo:rustc-link-search={}", output.join("lib").display());
 }
